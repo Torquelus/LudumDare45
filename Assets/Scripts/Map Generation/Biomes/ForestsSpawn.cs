d5e4cc303a4d0b0850237417a8d0d54c7d06a3b9
TaniyaPeters <36174064+TaniyaPeters@users.noreply.github.com>
2019-10-05 22:21:03 -0400

    Biomes Redone - comments added

--- Assets/Scripts/Map Generation/Biomes/ForestsSpawn.cs
+++ Assets/Scripts/Map Generation/Biomes/ForestsSpawn.cs
@@ -2,17 +2,44 @@
 
 public class ForestsSpawn : MonoBehaviour
 {
+	[Header("Objects In Forests")]
 	public GameObject[] Forests;
+
+	[Header("Number of Biome Objects")]
+	public int[] treeRange;
+
+	[Header("Biome Range X(min, max) Z(min, max)")]
+	public int[] biomeRange; 
 
 	// Start is called before the first frame update
 	void Start() {
-		int randForest = Random.Range(10, 20);
+
+		// Number of Trees and Twigs in forest
+		int randForest = Random.Range(treeRange[0], treeRange[1]);
+
+		// Random size of Biome from spawn point
+		int randWid = Random.Range(biomeRange[0], biomeRange[1]);
+		int randLen = Random.Range(biomeRange[2], biomeRange[3]);
+
+		// Draw All Images
 		for (int i = 0; i <= randForest; i++) {
+
+			// Choose Model to put into scene (e.g. Tree, twig)
 			int forestImg = Random.Range(0, Forests.Length);
-			int randX = Random.Range(-11, 11);
-			int randZ = Random.Range(-11, 11);
-			Vector3 newPos = new Vector3(transform.position.x + (randX * 10), transform.position.y, transform.position.z + (randZ * 10));
-			Instantiate(Forests[forestImg], newPos, Quaternion.identity);
+
+			// Random Position in range
+			int randX = Random.Range(-randWid, randWid);
+			int randZ = Random.Range(-randLen, randLen);
+
+			// Create the Position of new object 
+			Vector3 newPos = new Vector3(transform.position.x + (randX), 100, transform.position.z + (randZ));
+
+			// Find Ground - if Ground exists instantiate on ground
+			RaycastHit hit;
+			if (Physics.Raycast(newPos, Vector3.down, out hit)) {
+				Vector3 finalPos = hit.point;
+				Instantiate(Forests[forestImg], finalPos, Quaternion.identity);
+			}
 		}
 	}
 }
--- Assets/Scripts/Map Generation/Biomes/MeadowSpawn.cs
+++ Assets/Scripts/Map Generation/Biomes/MeadowSpawn.cs
@@ -1,20 +1,45 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class MeadowSpawn : MonoBehaviour
 {
+	[Header("Objects In Meadow")]
 	public GameObject[] Meadow;
+
+	[Header("Number of Biome Objects")]
+	public int[] meadowRange;
+
+	[Header("Biome Range X(min, max) Z(min, max)")]
+	public int[] biomeRange;
 
 	// Start is called before the first frame update
 	void Start() {
-		int randMeadow = Random.Range(5, 15);
+
+		// Number of objects in meadow
+		int randMeadow = Random.Range(meadowRange[0], meadowRange[1]);
+
+		// Random size of Biome from spawn point
+		int randWid = Random.Range(biomeRange[0], biomeRange[1]);
+		int randLen = Random.Range(biomeRange[2], biomeRange[3]);
+
+		// Draw All Images
 		for (int i = 0; i <= randMeadow; i++) {
+
+			// Choose Model to put into scene (e.g. stone, twig)
 			int meadowImg = Random.Range(0, Meadow.Length);
-			int randX = Random.Range(-11, 11);
-			int randZ = Random.Range(-11, 11);
-			Vector3 newPos = new Vector3(transform.position.x + (randX * 10), transform.position.y, transform.position.z + (randZ * 10));
-			Instantiate(Meadow[meadowImg], newPos, Quaternion.identity);
+
+			// Random Position in range
+			int randX = Random.Range(-randWid, randWid);
+			int randZ = Random.Range(-randLen, randLen);
+
+			// Create the Position of new object 
+			Vector3 newPos = new Vector3(transform.position.x + (randX), 100, transform.position.z + (randZ));
+
+			// Find Ground - if Ground exists instantiate on ground
+			RaycastHit hit;
+			if (Physics.Raycast(newPos, Vector3.down, out hit)) {
+				Vector3 finalPos = hit.point;
+				Instantiate(Meadow[meadowImg], finalPos, Quaternion.identity);
+			}
 		}
 	}
 }
