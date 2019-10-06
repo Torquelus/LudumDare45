bf07aa5b3a6029d38b5055dbce4b644a95f7a1e3
TaniyaPeters <36174064+TaniyaPeters@users.noreply.github.com>
2019-10-06 17:14:04 -0400

    get/set chara stats

--- Assets/Scripts/Character/CharacterMain.cs
+++ Assets/Scripts/Character/CharacterMain.cs
@@ -12,4 +12,63 @@ public class CharacterMain : MonoBehaviour {
 
 	public void Update() {
 	}
+
+	public void SetName(string newName) {
+		charaStat.name = newName;
+	}
+
+	public void IncStats(int stat, int amt) {
+		switch (stat) {
+			// Energy
+			case 0:
+				charaStat.energy += amt;
+				break;
+
+			// Health
+			case 1:
+				charaStat.health += amt;
+				break;
+
+			// Hunger
+			case 2:
+				charaStat.hunger += amt;
+				break;
+		}
+
+	}
+
+	public void DecStats(int stat, int amt) {
+		switch (stat) {
+			// Energy
+			case 0:
+				if (charaStat.energy > amt) {
+					charaStat.energy -= amt;
+				}
+				else {
+					charaStat.energy = 0;
+				}
+
+				break;
+
+			// Health
+			case 1:
+				if (charaStat.health > amt) {
+					charaStat.health -= amt;
+				}
+				else {
+					charaStat.health = 0;
+				}
+				break;
+
+			// Hunger
+			case 2:
+				if (charaStat.hunger> amt) {
+					charaStat.hunger -= amt;
+				}
+				else {
+					charaStat.hunger = 0;
+				}
+				break;
+		}
+	}
 }
