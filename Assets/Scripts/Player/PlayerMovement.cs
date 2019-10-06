3fa3f620a5e2f1b013ecfe8dee1a135b3049812c
vfewa <vfewa@users.noreply.github.com>
2019-10-06 16:15:01 -0400

    Selectable PlayerObject
    Changes material as well

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -7,6 +7,9 @@ public class PlayerMovement : MonoBehaviour
 {
     public Camera cam;
     private NavMeshAgent agent;
+    private bool selected = false;
+    public Material selectedcolor;
+    public Material deselectedcolor;
 
     private void Start()
     {
@@ -19,9 +22,26 @@ public class PlayerMovement : MonoBehaviour
     void Update()
     {
        
+        if(Input.GetMouseButtonDown(0)) //left click to select
+        {
+            Ray selectorray = cam.ScreenPointToRay(Input.mousePosition); //selectorray pointing to our selected unit
+            RaycastHit selected_unit;
 
-        if (Input.GetMouseButtonDown(1))
-        { //if left clicked
+            
+
+            if (Physics.Raycast(selectorray, out selected_unit)) //if there is a hit to a selectable object
+            {
+                if (selected_unit.collider.transform == this.transform) {//selects unit of it was hit
+                    selected = true;
+                    GetComponent<Renderer>().material = selectedcolor; ; //changes color
+                    
+
+                }
+            }
+
+        }
+        else if (Input.GetMouseButtonDown(1) && selected == true)
+        { //if right clicked and selected, the unit will move
 
             Ray ray = cam.ScreenPointToRay(Input.mousePosition); //ray is a Ray pointing to our cursor
             RaycastHit hit; //hit stores the point it hits on the NavMeshSurface
@@ -30,8 +50,13 @@ public class PlayerMovement : MonoBehaviour
             { 
                 //moves agent or player
                 agent.SetDestination(hit.point); //will pathfind player to hit
-            }
 
+            }
+            
+            selected = false;
+
+
+            GetComponent<Renderer>().material = deselectedcolor; ; //changes color
 
         }
 
